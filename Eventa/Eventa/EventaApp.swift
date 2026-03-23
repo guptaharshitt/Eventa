a9d2fdec559b2734fda7e14ff9c2c792d34aa3fb
Harshit <guptaharshit0105@gmail.com>
2026-03-23 12:28:06 +0530

    Nothing changed

---
+++ Eventa/Eventa/EventaApp.swift
@@ -0,0 +1,17 @@
+//
+//  EventaApp.swift
+//  Eventa
+//
+//  Created by HARSHIT on 23/03/26.
+//
+
+import SwiftUI
+
+@main
+struct EventaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
